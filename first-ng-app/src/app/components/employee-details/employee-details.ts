734ba4df24aee1d2f2811ae4e7296b2a05ba947d
reetikakushwaha <jireetika@gmail.com>
2025-10-25 20:03:31 +0530

    emp-details

--- first-ng-app/src/app/components/employee-details/employee-details.ts
+++ first-ng-app/src/app/components/employee-details/employee-details.ts
@@ -1,21 +1,45 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnDestroy, inject, SimpleChanges } from '@angular/core';
 import { Employee } from '../../model/employee';
+import { CommonModule } from '@angular/common';
+import { EventsApi } from '../../services/events-api';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-details.html',
   styleUrl: './employee-details.css',
 })
-export class EmployeeDetails {
+export class EmployeeDetails implements OnChanges, OnDestroy{
+  private _employeeApi = inject(EventsApi);
+  private _employeeApiSubsrcition:Subscription;
+  ngOnDestroy(): void {
+    if(this._employeeApiSubsrcition){
+      this._employeeApiSubsrcition.unsubscribe();
+    }
+  };
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this._employeeApi.getEmployeeDetails(this.employeeid).subscribe({
+      next: data => {
+        this.employee = data;
+
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+
   protected title: string = "Details of - ";
-  @Input() public employee:Employee ;
-  @Input() public subTitle:string ;
+  @Input() public employeeid: number;
+  @Input() public employee: Employee;
+  @Input() public subTitle: string;
 
   @Output() public sendConfirmationMessage: EventEmitter<string> = new EventEmitter<string>();
   
 
-  protected onEventProcessed():void {
+  protected onEventProcessed(): void {
     //fire an evnt to send data to parent component
     this.sendConfirmationMessage.emit(`The event ${this.employee.employeeName} has been processed successfully!`);
   }
--- first-ng-app/src/app/components/employee-list/employee-list.ts
+++ first-ng-app/src/app/components/employee-list/employee-list.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Employee } from '../../model/employee';
 import { EmployeeDetails } from '../employee-details/employee-details';
+import { Subscription } from 'rxjs';
+import { EventsApi } from '../../services/events-api';
+
 
 @Component({
   selector: 'app-employee-list',
@@ -11,56 +14,19 @@ import { EmployeeDetails } from '../employee-details/employee-details';
   templateUrl: './employee-list.html',
   styleUrls: ['./employee-list.css'],
 })
-export class EmployeeList {
+export class EmployeeList implements OnInit {
+  private _employeeServiceSubscription: Subscription;
+  private _employeeApi: EventsApi = inject(EventsApi);
   protected title: string = "Welcome to Bajaj finserv Employee List Component!";
   protected subTitle: string = "Subtitle - Welcome to Bajaj finserv Employee List Component!";
   protected columns: string[] = ["employee id", "employee name", "city", "phone", "show details"];
   protected selectedEmployee: Employee | null = null;
+   protected selectedEmployeeId: number;
   protected employeeChildmessage: string = '';
   protected searchChars: string = '';
   public childSubtitle: string = "Details of selected events!";
 
-  protected employee: Employee[] = [
-    {
-      employeeId: 2370,
-      employeeName: "Pravinkumar R. D.",
-      address: "Suncity, A8/404",
-      city: "Pune",
-      zipcode: 411051,
-      phone: "+91 23892893",
-      email: "pravin.r.d@synechron.com",
-      skillSets: "Microsoft/JavaScript",
-      country: "India",
-      joiningDate: new Date(),
-      avatar: "images/noimage.png"
-    },
-    {
-      employeeId: 2372,
-      employeeName: "Manish Kaushik",
-      address: "Mooncity, Z8/404",
-      city: "Raipur",
-      zipcode: 459899,
-      phone: "+91 9039039090",
-      email: "manish.kaushik@synechron.com",
-      skillSets: "DBA",
-      country: "India",
-      joiningDate: new Date(),
-      avatar: "images/noimage.png"
-    },
-    {
-      employeeId: 2374,
-      employeeName: "Alisha C.",
-      address: "Mooncity, B8/404",
-      city: "Mumbai",
-      zipcode: 510512,
-      phone: "+91 30003000",
-      email: "alisha.c@synechron.com",
-      skillSets: "Java",
-      country: "India",
-      joiningDate: new Date(),
-      avatar: "images/noimage.png"
-    }
-  ];
+  protected employee: Employee[] = []
 
   protected filteredEmployee: Employee[] = [...this.employee];
 
@@ -74,12 +40,27 @@ export class EmployeeList {
       console.log(this.filteredEmployee);
   }
 
-  protected onSelectedEmployee(employee: Employee): void {
-    console.log("Employee selected: ", employee);
-    this.selectedEmployee = employee;
+  protected onSelectedEmployee(id: number): void {
+    // console.log("Employee selected: ", employee);
+    this.selectedEmployeeId = id;
   }
 
   protected handleEmployeeChildMessage(message: string): void {
     this.employeeChildmessage = message;
+  }
+  
+  ngOnInit(): void {
+    
+    this._employeeServiceSubscription = this._employeeApi.getAllEmployees().subscribe({
+      next: employeeData => {
+        this.employee = employeeData;
+        this.filteredEmployee = [...this.employee];
+        console.log("Employee data received: ", employeeData);
+      },
+      error: err => {
+        console.error("Error fetching employee data: ", err);
+      }
+    });
+  
   }
 }
